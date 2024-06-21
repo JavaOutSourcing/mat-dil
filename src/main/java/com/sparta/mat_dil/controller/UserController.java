2814a95a2639737a55a9f9ef340d0b989de19899
harvye11010 <hunc0416@gmail.com>
2024-06-21 10:33:50 +0900

    [Feat] statusCode#6

--- src/main/java/com/sparta/mat_dil/controller/UserController.java
+++ src/main/java/com/sparta/mat_dil/controller/UserController.java
@@ -2,7 +2,9 @@ package com.sparta.mat_dil.controller;
 
 import com.sparta.mat_dil.dto.ProfileRequestDto;
 import com.sparta.mat_dil.dto.ProfileResponseDto;
+import com.sparta.mat_dil.dto.ResponseMessageDto;
 import com.sparta.mat_dil.service.UserService;
+import com.sparta.mat_dil.enums.ResponseStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,23 +16,35 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/{userId}")
-//    @GetMapping
-    public ResponseEntity<ProfileResponseDto> getProfile(@PathVariable Long userId) {
-//            @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return ResponseEntity.ok().body(userService.getProfile(userId));
-//        return ResponseEntity.ok().body(userService.getProfile(userDetails.getUser().getId()));
+    //시큐리티 적용 전
+/*    @GetMapping("/{userId}")
+    public ResponseEntity<ResponseMessageDto> getProfile(@PathVariable Long userId) {
+        userService.getProfile(userId);
+        return ResponseEntity.ok(new ResponseMessageDto(ResponseStatus.PROFILE_CHECK_SUCCESS));
+    }*/
+
+    @GetMapping
+    public ResponseEntity<ResponseMessageDto> getProfile(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        userService.getProfile(userDetails.getUser().getId());
+        return ResponseEntity.ok(new ResponseMessageDto(ResponseStatus.PROFILE_CHECK_SUCCESS));
     }
 
-//    @PatchMapping
-    @PatchMapping("/{userId}")
-    public ResponseEntity<ProfileResponseDto> profileUpdate(
-//            @AuthenticationPrincipal UserDetailsImpl userDetails,
+    //시큐리티 적용 전
+ /*   @PutMapping("/{userId}")
+    public ResponseEntity<ResponseMessageDto> profileUpdate(
             @PathVariable Long userId,
             @RequestBody ProfileRequestDto requestDto) {
+        userService.update(userId, requestDto);
+        return ResponseEntity.ok(new ResponseMessageDto(ResponseStatus.PROFILE_UPDATE_SUCCESS));
+    }*/
 
-//        return ResponseEntity.ok().body(userService.update(userDetails.getUser().getId(), requestDto));
-        return ResponseEntity.ok().body(userService.update(userId, requestDto));
+    //시큐리티 적용 시
+    @PatchMapping
+    public ResponseEntity<ResponseMessageDto> profile1Update(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestBody ProfileRequestDto requestDto) {
+        userService.update(userDetails.getUser().getId(), requestDto);
+        return ResponseEntity.ok(new ResponseMessageDto(ResponseStatus.PROFILE_UPDATE_SUCCESS));
     }
 
 
